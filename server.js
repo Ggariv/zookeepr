9f7c6b61729c00580711f4e8f4ee6b5b80058bc5
Gonzalo <ggarciarivera09@gmail.com>
2022-12-14 19:28:31 -0500

    feature

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -84,6 +83,22 @@ app.get('/api/animals/:id', (req, res) => {
     else {
         res.send(404);
         }
+    });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+    });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+    });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
     });
 
 app.post('/api/animals', (req, res) => {
